0399b8204d7e69f1eacdc733fd077b199959536c
K.Thorpe <kieran123416@gmail.com>
2023-06-05 20:35:24 +0100

    added a dash attack

---
+++ Source/Side_Scroller_2D.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Side_Scroller_2DTarget : TargetRules
+{
+	public Side_Scroller_2DTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		ExtraModuleNames.AddRange( new string[] { "Side_Scroller_2D" } );
+	}
+}
---
+++ Source/Side_Scroller_2DEditor.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Side_Scroller_2DEditorTarget : TargetRules
+{
+	public Side_Scroller_2DEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		ExtraModuleNames.AddRange( new string[] { "Side_Scroller_2D" } );
+	}
+}
